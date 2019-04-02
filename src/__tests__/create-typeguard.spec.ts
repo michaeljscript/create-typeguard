17ddcba1edf645d345d08becc0f4fdcd7fb4117b
Michal Szorád <miso.szorad@gmail.com>
2019-04-02 20:20:03 +0200

    Refactor and add some tests

---
+++ src/__tests__/create-typeguard.spec.ts
@@ -0,0 +1,14 @@
+import { hasProperties } from '../index';
+
+describe('create-typeguard', () => {
+
+    describe('hasProperties', () => {
+        it('should return false when object has not any of the given property', () => {
+            expect(hasProperties({ foo: 'bar' }, 'foo', 'test')).toBe(false);
+        });
+
+        it('should return true when the object has all of the properties', () => {
+            expect(hasProperties({ foo: 'bar', bar: 'foo' }, 'foo', 'bar')).toBe(true);
+        });
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 type Parser<T> = (value: unknown) => T | null;
 
 function hasProperties<T extends object, K extends string>(obj: T, ...keys: K[]): obj is T & { [J in K]: unknown } {
-    return !!obj && !keys.some(key => !obj.hasOwnProperty(key));
+    return !!obj && keys.every(key => obj.hasOwnProperty(key));
 }
 
 
@@ -9,5 +9,5 @@ const createTypeGuard = <T>(parse: Parser<T>) => (value: unknown): value is T =>
     return parse(value) !== null;
 };
 
-export {hasProperties, createTypeGuard};
+export { hasProperties, createTypeGuard };
 export default createTypeGuard;
