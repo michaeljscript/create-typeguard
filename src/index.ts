ed751ed25893fe070bf2fe9015b20faa06635574
Michal Szorád <miso.szorad@gmail.com>
2019-04-07 18:17:05 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,48 @@
-type Parser<T> = (value: unknown) => T | null;
+type Parser<OUTPUT, INPUT> = (value: INPUT) => OUTPUT | null;
 
 function hasProperties<T extends object, K extends string>(obj: T, ...keys: K[]): obj is T & { [J in K]: unknown } {
     return !!obj && keys.every(key => obj.hasOwnProperty(key));
 }
 
 
-const createTypeGuard = <T>(parse: Parser<T>) => (value: unknown): value is T => {
+const createTypeGuard = <T, K = unknown>(parse: Parser<T, K>) => (value: K): value is K & T => {
     return parse(value) !== null;
 };
+
+enum PersonType {
+    ADMIN = 'ADMIN',
+    SOMEONE = 'SOMEONE',
+};
+
+interface Person {
+    readonly type: PersonType;
+    age: number;
+}
+
+interface Admin extends Person {
+    readonly type: PersonType.ADMIN;
+}
+
+const a: Person = { age: 1, type: PersonType.SOMEONE };
+
+const isAdmin = createTypeGuard<Admin, Person>(p => {
+
+    switch (p.type) {
+        case PersonType.ADMIN:
+            return {
+                age: p.age,
+                type: p.type,
+
+            }
+    }
+
+    return null;
+});
+
+
+if (isAdmin(a)) {
+    // a.type === PersonType.ADMIN
+}
 
 export { hasProperties, createTypeGuard };
 export default createTypeGuard;
