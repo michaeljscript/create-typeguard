f00e5144d1b3068db612ba3a32e56f2b2d94f73e
Michal Szorád <miso.szorad@gmail.com>
2019-04-24 19:31:37 +0200

    Add more examples

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,87 @@
 type Parser<OUTPUT, INPUT> = (value: INPUT) => OUTPUT | null;
 
 function hasProperties<T extends object, K extends string>(obj: T, ...keys: K[]): obj is T & { [J in K]: unknown } {
-    return !!obj && keys.every(key => obj.hasOwnProperty(key));
+  return !!obj && keys.every(key => obj.hasOwnProperty(key));
 }
 
-
-const createTypeGuard = <T, K = unknown>(parse: Parser<T, K>) => (value: K): value is K & T => {
-    return parse(value) !== null;
+const createTypeGuard = <Type extends INPUT, INPUT = unknown>(parse: Parser<Type, INPUT>) => (value: INPUT): value is Type => {
+  return parse(value) !== null;
 };
 
 enum PersonType {
-    ADMIN = 'ADMIN',
-    SOMEONE = 'SOMEONE',
-};
+  ADMIN = "ADMIN",
+  SOMEONE = "SOMEONE"
+}
 
 interface Person {
-    readonly type: PersonType;
-    age: number;
+  readonly type: PersonType;
+  age: number;
 }
 
 interface Admin extends Person {
-    readonly type: PersonType.ADMIN;
+  readonly type: PersonType.ADMIN;
 }
 
 const a: Person = { age: 1, type: PersonType.SOMEONE };
 
-const isAdmin = createTypeGuard<Admin, Person>(p => {
-
-    switch (p.type) {
-        case PersonType.ADMIN:
-            return {
-                age: p.age,
-                type: p.type,
-
-            }
+function parsePerson(value: unknown): Person | null {
+  if (typeof value === "object" && value) {
+    if (hasProperties(value, "type", "age")) {
+      const { type, age } = value;
+      if (typeof age === "number" && typeof type === "string") {
+        if (type === PersonType.ADMIN || type === PersonType.SOMEONE) {
+          return { type, age };
+        }
+      }
     }
+  }
 
-    return null;
+  return null;
+}
+
+const isPerson2 = createTypeGuard(parsePerson);
+
+const isPerson = createTypeGuard<Person>(value => {
+  if (typeof value === "object" && value) {
+    if (hasProperties(value, "type", "age")) {
+      const { type, age } = value;
+      if (typeof age === "number" && typeof type === "string") {
+        if (type === PersonType.ADMIN || type === PersonType.SOMEONE) {
+          return { type, age };
+        }
+      }
+    }
+  }
+
+  return null;
 });
 
+const isAdmin = createTypeGuard<Admin, Person>(p => {
+  switch (p.type) {
+    case PersonType.ADMIN:
+      return {
+        age: p.age,
+        type: p.type
+      };
+  }
+
+  return null;
+});
+
+const isAdmin2 = createTypeGuard<Admin, Person>(p => {
+  switch (p.type) {
+    case PersonType.ADMIN:
+      return {
+        age: p.age,
+        type: p.type
+      };
+  }
+
+  return null;
+});
 
 if (isAdmin(a)) {
-    // a.type === PersonType.ADMIN
+  // a.type === PersonType.ADMIN
 }
 
 export { hasProperties, createTypeGuard };
