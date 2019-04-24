e71b0b9fa604763bc00671ee886dc2454448ab4a
Michal Szorád <miso.szorad@gmail.com>
2019-04-24 22:06:05 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ const createTypeGuard = <Type extends INPUT, INPUT = unknown>(parse: Parser<Type
 enum PersonType {
   ADMIN = "ADMIN",
   SOMEONE = "SOMEONE"
+  //   C = "C"
 }
 
 interface Person {
@@ -24,12 +25,74 @@ interface Admin extends Person {
 
 const a: Person = { age: 1, type: PersonType.SOMEONE };
 
+type IsNever<T> = T extends never ? true : never;
+
+function asdasxa<T extends keyof typeof PersonType>(b: T[], a: Exclude<keyof typeof PersonType, T> extends never ? true : never) {}
+
+function asdasxa2<T extends PersonType>(b: T[], a: Exclude<PersonType, T> extends never ? true : never) {}
+
+asdasxa2([PersonType.ADMIN, PersonType.SOMEONE], true);
+
+const createEnumParser = <Enum>() => <Value extends Enum>(b: Value[], a: Exclude<Enum, Value> extends never ? true : never) => {
+  return (value: unknown): Enum | null => {
+    for (const enumValue of b) {
+      if (enumValue === value) {
+        return enumValue;
+      }
+    }
+    return null;
+  };
+};
+
+const parsePersonType = createEnumParser<PersonType>()([PersonType.ADMIN, PersonType.SOMEONE], true);
+
+const isPersonTypeSafe = createTypeGuard(parsePersonType);
+
+// type EQ<A, B> = A extends B ? (B extends A ? true : never) : never;
+
+// function test<A, B>(a: A, b: B): boolean {
+//   return true;
+// }
+
+// type All<T> = Exclude<keyof typeof PersonType, T> extends never ? T : never;
+// const qweqweasxxa: Exclude<keyof typeof PersonType, "ADMIN" | "SOMEONE" | "aaa"> extends never ? true : never = true;
+
+// const qweqweqweq: EQ<Admin, Admin> = true;
+
+// const aasdaxa: typeof PersonType = [];
+
+// type AAA = Exclude<"ADMIN", "ADMIN">;
+
+// const qweqweqw: EQ<Exclude<"ADMIN", "ADMIN">, never> = true;
+
+// const aasdas: All<PersonType.ADMIN | PersonType.SOMEONE> = PersonType.ADMIN;
+
+// function abc<T>(b: All<T>): boolean {
+//   return true;
+// }
+
+// const createEnumTypeGuard = <E>(obj: object) => (val: unknown): val is E => {
+//   return true;
+// };
+
+const isPersonType = createTypeGuard<PersonType>(value => {
+  const keys: PersonType[] = [];
+
+  switch (value) {
+    case PersonType.ADMIN:
+    case PersonType.SOMEONE:
+      return value;
+  }
+
+  return null;
+});
+
 function parsePerson(value: unknown): Person | null {
   if (typeof value === "object" && value) {
     if (hasProperties(value, "type", "age")) {
       const { type, age } = value;
       if (typeof age === "number" && typeof type === "string") {
-        if (type === PersonType.ADMIN || type === PersonType.SOMEONE) {
+        if (isPersonType(type)) {
           return { type, age };
         }
       }
