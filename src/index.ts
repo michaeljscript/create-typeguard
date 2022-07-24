e02a62912337cd055cb64d2781084e5cddce1a14
akolarski <37934108+akolarski@users.noreply.github.com>
2022-07-24 23:14:55 +0300

    A better approach is for the parser to require for the whole to be defined.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-type Parser<T> = (value: unknown) => T | null;
+type Parser<T> = (value: unknown) => Required<T> | null;
 
 function hasProperties<T extends object, K extends string>(obj: T, ...keys: K[]): obj is T & { [J in K]: unknown } {
     return !!obj && keys.every(key => obj.hasOwnProperty(key));
 }
 
 
-const createTypeGuard = <T>(parse: Parser<Required<T>>) => (value: unknown): value is T => {
+const createTypeGuard = <T>(parse: Parser<T>) => (value: unknown): value is T => {
     return parse(value) !== null;
 };
 
